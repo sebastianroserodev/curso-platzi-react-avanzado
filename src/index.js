2d6c90fac7bb224e8c1c0ea919e48e5017c1df8d
Sebastian Rosero Garcia <sebastian.rosero.dev@gmail.com>
2023-05-23 16:01:14 -0500

    Configuración prettier (formateador) y ESlint (Sintaxis)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('empezamos')
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const container = document.getElementById('app')
+const root = createRoot(container)
+root.render(<h1>Hola platzi</h1>)
